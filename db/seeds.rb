0b80d7201555713058c137d39b31cb6285574332
Ryan Lesson & Eric Shou & Jay Yee <pair+bigless27.eshou1014.formirthonly@devbootcamp.com>
2015-12-11 17:21:51 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@ jay.password = "jayjayjay"
 #jay.password_confirmation = "jayjayjay"
 jay.save
 
-js = Survey.create(user_id: jay.id, name: "jsurvey")
+js = Suvey.create(user_id: jay.id, name: "jsurvey")
 jq = Question.create(question: "What is your favorite color", survey_id: jq.id)
 jc = Choice.create(question_id: jq.id, a: "purple", b: "black", c: "magenta", d: "red")
