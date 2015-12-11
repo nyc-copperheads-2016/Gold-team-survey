d4df1bda2e3b83377a051542c0160f56d514725b
eshou1014 <eshou1014@gmail.com>
2015-12-11 16:54:33 -0500

    Added survey index. Incomplete

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,7 +1,16 @@
+get '/surveys' do
+  erb :'surveys/index'
+end
+
 get '/surveys/new' do
   erb :'surveys/new'
 end
 
 post '/surveys' do
-  new_survey = Survey.new(name: params[:name])
+  new_survey = Survey.new(name: params[:name], creator: current_user)
+  if new_survey.save
+    redirect '/'
+  else
+    erb :'surveys/new'
+  end
 end
