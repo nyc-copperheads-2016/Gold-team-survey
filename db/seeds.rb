9f12a29498e332105fae9773186bc21b8e1b9c9d
Jay Yee <fmo@opmbx.org>
2015-12-12 14:17:30 -0500

    Start changing take survey route

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,7 @@ js = Survey.create(user_id: jay.id, name: "jsurvey")
 js = Survey.find_by(user_id: jay.id)
 jq = Question.create(question: "What is your favorite color", survey_id: js.id)
 jc = Choice.create(question_id: jq.id, a: "purple", b: "black", c: "magenta", d: "red")
+
+jq2 = Question.create(question: "What is your favorite number", survey_id: js.id)
 jr = Round.create(user_id: jay.id, survey_id: js.id)
-jq.decisions.create(decision: "a", round_id: jr.id)
+# jq.decisions.create(decision: "a", round_id: jr.id)
