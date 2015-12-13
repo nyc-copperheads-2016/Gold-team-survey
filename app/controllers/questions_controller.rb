8ae681683a4a9975fa4be816441a1b9b8710048f
Ryan Lesson & Eric Shou & Jay Yee <pair+bigless27.eshou1014.formirthonly@devbootcamp.com>
2015-12-12 21:56:24 -0500

    add create user functionality without ajax

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,21 @@
-get '/questions/new' do
-  erb :'questions/new'
+get '/surveys/:id/questions/new' do
+  survey=Survey.find_by(id:params[:id])
+  if request.xhr?
+    erb :'surveys/show',locals:{survey:survey}
+  else
+    erb :'surveys/_surveys',locals:{survey:survey}
+  end
 end
 
 post '/questions' do
-  new_questions = Question.new(question: params[:question], survey_id: params[:survey_id])
-  if new_questions.save
-    redirect '/choices/new'
+  current_survey = Survey.find(params[:survey_id])
+  current_q = current_survey.questions.new(question: params[:question])
+  current_c=current_q.choices.new(params[:choice])
+
+  # need to write validations for user unfilled/wacky submissions
+
+  if current_q.save && current_c.save
+    redirect "/surveys/#{params[:survey_id]}/questions/new"
   else
     erb :'questions/new'
   end
@@ -16,6 +26,6 @@ get '/rounds/:round_id/questions/:id' do
   if request.xhr?
     erb :'question/show'
   else
-    erb :'questions/_question', layout: true, locals: { round_id: params[:round_id], question: question }
+    erb :'questions/_question', locals: { round_id: params[:round_id], question: question }
   end
 end
