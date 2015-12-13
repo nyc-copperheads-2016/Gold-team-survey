cbdfa3bad9c5cf6481fa353be2a6126f1427678c
Ryan Lesson & Eric Shou & Jay Yee <pair+bigless27.eshou1014.formirthonly@devbootcamp.com>
2015-12-12 20:34:56 -0500

    Delete extraneous code

--- public/js/application.js
+++ public/js/application.js
@@ -1,16 +1,12 @@
 $(document).ready(function() {
     $('.form-container').on('submit', 'form', function(e) {
-      // console.log("before")
       e.preventDefault();
-      console.log("after")
       $.ajax({
         method: 'post',
         url: e.target.action,
         data: $(e.target).serialize()
       }).done(function(response){
-        // debugger
         $('.form-container').html(response);
-        // $('.bigger-container').append(response);
       }).fail(function(response){
         console.log("There was an error: " + response);
       });
